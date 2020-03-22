bdc0a12a7021a01fab9363bb7f5d6847a6350e56
Arka Saha <40143090+ArkaJU@users.noreply.github.com>
2020-03-22 14:16:29 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,11 @@
+#Import svm model
+from sklearn import svm
+
+#Create a svm Classifier
+clf = svm.SVC(kernel='linear') # Linear Kernel
+
+#Train the model using the training sets
+clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
